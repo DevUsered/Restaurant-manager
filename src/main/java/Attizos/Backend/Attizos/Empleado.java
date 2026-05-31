f6f1733342c8f4bb62bc89a2874a0c7979515850
Tu Nombre <tu-email@ejemplo.com>
2026-05-31 09:01:45 -0400

    Eliminar clases de usuario y productos no utilizados para simplificar el código

--- src/main/java/Attizos/Backend/Attizos/Empleado.java
+++ src/main/java/Attizos/Backend/Attizos/Empleado.java
@@ -1,56 +1,77 @@
 package Attizos.Backend.Attizos;
 
 public class Empleado {
-    private String id;
+    private String idEmpleado;
     private String nombre;
     private String cargo;
     private double sueldo;
+    private String estado;
+
+    private String username;
+    private String passwordHash;
 
     // Constructor
-    public Empleado(String id, String nombre, String cargo, double sueldo) {
-        this.id = id;
+    public Empleado(){}
+    public Empleado(String id, String nombre, String cargo, double sueldo, String estado, String username, String passwordHash) {
+        this.idEmpleado = id;
         this.nombre = nombre;
         this.cargo = cargo;
         this.sueldo = sueldo;
-    }
-    public Empleado(){
-    }
-
-    // --- GETTERS ---
-    public String getId() {
-        return id;
+        this.estado = estado;
+        this.username = username;
+        this.passwordHash = passwordHash;
     }
 
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
     public String getNombre() {
         return nombre;
     }
-
     public String getCargo() {
         return cargo;
     }
-
     public double getSueldo() {
         return sueldo;
     }
-    public void setId(String id) {
-        this.id = id;
+    public String getEstado() {
+        return estado;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPasswordHash() {
+        return passwordHash;
     }
 
+    //SETERS
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
-
-    public void setSueldo(double sueldo) {
+    public void setSueldo(double sueldo) {        
         this.sueldo = sueldo;
     }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public boolean tieneAccesoSistema(){
+        return this.username != null && !this.username.trim().isEmpty();
+    }
     @Override
-    public String toString() {
-        // String.format ayuda a que las columnas se vean alineadas en la consola
-        return String.format("ID: [%d] | Nombre: %-15s | Cargo: %-15s | Sueldo: Bs. %.2f",
-                id, nombre, cargo, sueldo);
+    public String toString(){
+        return nombre + " ("+cargo +")";
     }
 }
